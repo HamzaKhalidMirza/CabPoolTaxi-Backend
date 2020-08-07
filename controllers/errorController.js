cee82cd1a74edf14564408feb18b448d3327af04
HamzaKhalidMirza <hamzakhalid.zx@gmail.com>
2020-08-07 13:35:20 +0500

    Setup

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -90,16 +90,15 @@ module.exports = (err, req, res, next) => {
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, req, res);
   } else if (process.env.NODE_ENV.trim() === 'production') {
-    sendErrorDev(err, req, res);
-    // let error = { ...err };
-    // error.message = err.message;
+    let error = { ...err };
+    error.message = err.message;
 
-    // if (error.name === 'CastError') error = handleCastErrorDB(error);
-    // if (error.code === 11000) error = handleDuplicateFieldsDB(error);
-    // if (error.name === 'ValidationError') error = handleValidationErrorDB(error);
-    // if (error.name === 'JsonWebTokenError') error = handleJWTError();
-    // if (error.name === 'TokenExpiredError') error = handleJWTExpiredError();
+    if (error.name === 'CastError') error = handleCastErrorDB(error);
+    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+    if (error.name === 'ValidationError') error = handleValidationErrorDB(error);
+    if (error.name === 'JsonWebTokenError') error = handleJWTError();
+    if (error.name === 'TokenExpiredError') error = handleJWTExpiredError();
 
-    // sendErrorProd(error, req, res);
+    sendErrorProd(error, req, res);
   }
 };
